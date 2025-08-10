94d22a9b2ffc1909029cbca155408af48e3dd657
chinky23-tech <chinky0330@gmail.com>
2025-08-10 11:29:07 +0530

    countdown function

--- script.js
+++ script.js
@@ -18,3 +18,34 @@ const timezoneOffset = now.getTimezoneOffset() * 60000;
 
 const localISOTime = (new Date(now - timezoneOffset)).toISOString().slice(0,16);
 dateTimeInput.min = localISOTime;
+
+startBtn.addEventListener('click' , startCountdown);
+resetBtn.addEventListener('click', resetBtn);
+
+
+function startCountdown(){
+    const selectedDateTime = dateTimeInput.value;
+
+    if(!selectedDateTime){
+        alert('Please select a date and time');
+        return;
+    }
+    //clear any existing countdown
+
+    if(countdownInterval){
+        clearInterval(countdownInterval);
+    }
+    // hide msg if shown
+    messageElement.classList.remove('active');
+    
+    //calculate the target time (convert from local to UTC)
+
+    const targetTime = new Date(selectedDateTime).getTime();
+
+    //update countdown immediately
+
+    updateCountdown(targetTime);
+
+    //Then update every second
+    countdownInterval = setInterval(() => updateCountdown(targetTime), 1000); 
+}
